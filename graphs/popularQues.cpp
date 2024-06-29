bcc87474d5f4430446e5ccf0a1481506ba53a5bc
Yash Gupta <yash.gupta0808@gmail.com>
2024-06-29 21:00:38 +0530

    add new ques

--- graphs/popularQues.cpp
+++ graphs/popularQues.cpp
@@ -5,13 +5,17 @@ using namespace std;
 class courseSchedule
 {
 public:
+    void addEdge(vector<vector<int>> &adjList, int u, int v)
+    {
+        adjList[u].push_back(v);
+    }
     bool canFinish(int vertices, vector<vector<int>> &prerequisites)
     {
-        vector<int> adjList[vertices];
+        vector<vector<int>> adjList(vertices);
         vector<int> inDegree(vertices, 0); // incoming edges to a node
         for (auto i : prerequisites)
         {
-            adjList[i[1]].push_back(i[0]);
+            addEdge(adjList, i[1], i[0]);
             inDegree[i[0]]++;
         }
 
@@ -38,5 +42,87 @@ public:
             }
         }
         return ans.size() == vertices;
+    }
+};
+
+// https://leetcode.com/problems/01-matrix/description/
+class matrix01
+{
+public:
+    vector<vector<int>> updateMatrix(vector<vector<int>> &matrix)
+    {
+        int rows = matrix.size();
+        int cols = matrix[0].size();
+        vector<vector<int>> ans(rows, vector<int>(cols, INT_MAX));
+        queue<pair<int, int>> q;
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < cols; j++)
+            {
+                if (matrix[i][j] == 0)
+                {
+                    ans[i][j] = 0;
+                    q.push({i, j});
+                }
+            }
+        }
+        vector<pair<int, int>> dirs = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+        while (!q.empty())
+        {
+            pair<int, int> node = q.front();
+            q.pop();
+            for (auto dir : dirs)
+            {
+                int x = node.first + dir.first;
+                int y = node.second + dir.second;
+                if (x < 0 || x >= rows || y < 0 || y >= cols || ans[x][y] <= ans[node.first][node.second] + 1)
+                    continue;
+                ans[x][y] = ans[node.first][node.second] + 1;
+                q.push({x, y});
+            }
+        }
+        return ans;
+    }
+};
+
+// https://leetcode.com/problems/number-of-provinces/description/
+class numberOfProvinces
+{
+public:
+    void dfs(vector<bool> &vis, int startNode, vector<vector<int>> &adjList)
+    {
+        vis[startNode] = 1;
+        for (auto e : adjList[startNode])
+        {
+            if (!vis[e])
+            {
+                dfs(vis, e, adjList);
+            }
+        }
+    }
+
+    int findCircleNum(vector<vector<int>> &isConnected)
+    {
+        vector<vector<int>> adjList(isConnected.size());
+        for (int i = 0; i < isConnected.size(); i++)
+        {
+            for (int j = 0; j < isConnected.size(); j++)
+            {
+                if (isConnected[i][j])
+                    adjList[i].push_back(j);
+            }
+        }
+        vector<bool> vis(isConnected.size(), 0);
+        int ans = 0;
+        for (int i = 0; i < isConnected.size(); i++)
+        {
+
+            if (!vis[i])
+            {
+                dfs(vis, i, adjList);
+                ans++;
+            }
+        }
+        return ans;
     }
 };
